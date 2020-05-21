77dbf49922769a9b79b8b1a7a289e3b6d031c755
Eric <Aerelioyanadel@gmail.com>
2020-05-21 10:58:56 -0400

    initial commit with all dependencies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch("/deck").then((res) => res.json())
+      setData(result)
+    }
+
+    fetchData()
+    console.log(data.deck_id)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      this is home
+      <div>
+        <h1>{data.deck_id}</h1>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
