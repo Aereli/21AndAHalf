3935f63463928e3940c182c765527ae690cbc1c2
Eric <Aerelioyanadel@gmail.com>
2020-06-16 19:47:23 -0400

    add snapshot dragOver color

--- src/components/Table/column.js
+++ src/components/Table/column.js
@@ -6,12 +6,15 @@ import { Droppable } from "react-beautiful-dnd"
 const Container = styled.div`
   border: 1px solid grey;
   /* width: 300px; */
+  display: flex;
+  flex-direction: column;
 `
 const Title = styled.div`
   padding: 8px;
 `
 const TaskList = styled.div`
   padding: 8px;
+  flex-grow: 1;
 `
 const Column = (props) => {
   return (
@@ -19,8 +22,15 @@ const Column = (props) => {
       <Container>
         <Title>{props.column.name}</Title>
         <Droppable droppableId={props.column.id}>
-          {(provided) => (
-            <TaskList ref={provided.innerRef} {...provided.droppableProps}>
+          {(provided, snapshot) => (
+            <TaskList
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              style={{
+                background: snapshot.isDraggingOver ? `#354a21` : `white`,
+                // ...provided.draggableProps.style,
+              }}
+            >
               {props.tasks.map((task, index) => {
                 return <Task key={task.id} task={task} index={index} />
               })}
--- src/components/Table/task.js
+++ src/components/Table/task.js
@@ -11,11 +11,15 @@ const Task = (props) => {
   return (
     <>
       <Draggable draggableId={props.task.id} index={props.index}>
-        {(provided) => (
+        {(provided, snapshot) => (
           <Container
             {...provided.draggableProps}
             {...provided.dragHandleProps}
             ref={provided.innerRef}
+            style={{
+              background: snapshot.isDragging ? `#99ba83` : `white`,
+              ...provided.draggableProps.style,
+            }}
           >
             {props.task.content}
           </Container>
