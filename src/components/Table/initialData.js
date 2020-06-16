b92dc51377f70bf03fca295156ca278a74ef4e3a
Eric <Aerelioyanadel@gmail.com>
2020-06-16 19:23:18 -0400

    update initial data

--- src/components/Table/initialData.js
+++ src/components/Table/initialData.js
@@ -8,11 +8,26 @@ const data = {
   columns: {
     column1: {
       id: "column1",
-      name: "table",
+      name: "table1",
       taskIds: ["task1", "task2", "task3"],
     },
+    column2: {
+      id: "column2",
+      name: "table2",
+      taskIds: [],
+    },
+    column3: {
+      id: "column3",
+      name: "table3",
+      taskIds: [],
+    },
+    column4: {
+      id: "column4",
+      name: "table4",
+      taskIds: [],
+    },
   },
-  columnsOrder: ["column1"],
+  columnsOrder: ["column1", "column2", "column3", "column4"],
 }
 
 export default data
